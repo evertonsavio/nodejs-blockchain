0da5bffc1465fcf03a42873cd86c16546d4b9ec8
evertonsavio <everluca@outlook.com>
2022-03-01 10:15:28 -0300

    :sparkles: minning a block

--- cryptochain/block.test.js
+++ cryptochain/block.test.js
@@ -32,3 +32,26 @@ describe('genesis()', () => {
     })
 
 });
+
+describe('mineBlock()', () => {
+    const lastBlock = Block.genesis();
+    const data = 'mined data';
+    const minedBlock = Block.mineBlock({ lastBlock, data });
+
+    it('returns a Block instance', () => {
+        expect(minedBlock instanceof Block).toBe(true);
+    });
+
+    it('sets the `lastHash` to be the hash of the lastBlock', () => {
+        expect(minedBlock.lastHash).toEqual(lastBlock.hash);
+    })
+
+    it('sets the `data`', () => {
+        expect(minedBlock.data).toEqual(data);
+    })
+
+    it('sets the `timestamp`', () => {
+        expect(minedBlock.timestamp).not.toEqual(undefined);
+    })
+
+})
