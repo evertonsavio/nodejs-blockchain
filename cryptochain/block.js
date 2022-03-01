a976387caee22ebf19ac8046abaade074abeea53
evertonsavio <everluca@outlook.com>
2022-03-01 09:44:34 -0300

    :sparkles: jest tdd

--- cryptochain/block.js
+++ cryptochain/block.js
@@ -1,4 +1,5 @@
 class Block {
+
     constructor({ timestamp, lastHash, hash, data }) {
         this.timestamp = timestamp;
         this.lastHash = lastHash;
@@ -7,11 +8,4 @@ class Block {
     }
 }
 
-const block1 = new Block({
-    timestamp: '01/01/01',
-    lastHash: 'foo-last-hash',
-    hash: 'foo-hash',
-    data: 'foo-data'
-});
-
-console.log('block1', block1);
+module.exports = Block;
